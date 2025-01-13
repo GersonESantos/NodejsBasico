6bc9dc9c1377439a429485704ee34302eb4f27c5
GersonESS <gebhsantos@gmail.com>
2025-01-12 22:08:01 -0300

    a

--- 07express-handlebars/app.js
+++ 07express-handlebars/app.js
@@ -1,14 +1,29 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-
-const app = express();
-
-app.engine('handlebars', engine());
+// Importando o Express e o Express-handlebars
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+// App
+var app = express();
+ 
+// Template
+app.engine('handlebars', exphbs({defaultLayout : 'principal'}));
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+ 
+// Rotas
+app.get('/', function (req, res) {
 
-app.get('/', (req, res) => {
-    res.render('home');
+    let pessoas = [
+        {"nome":"Ralf", "idade":30},
+        {"nome":"Mayra", "idade":31},
+        {"nome":"Henrique", "idade":28}
+    ]
+
+    res.render('inicio', {gostandoDeNode:false, dados:pessoas});
 });
 
+app.get('/sobre', function (req, res) {
+    res.render('sobre');
+});
+ 
+// Servidor
 app.listen(8080);
